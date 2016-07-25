94118f2f1b021a66f63c49597c5d78a119327832
maryamelham91 <maryamelham7@gmail.com>
2016-07-25 09:01:46 +0300

    Binary Search done

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,6 +11,37 @@
  */
 
 var binarySearch = function (array, target) {
-  
+	//solution not in binarySearch
+	// for (var i = 0; i < array.length; i++) {
+	// 	if(target===array[i])
+	// 		return i ;
+
+ //  }
 };
 
+//As binary search algorithm divide the array and check between min adn max 
+var binarySearch = function (array, target) {
+    var min = 0;
+    var max = array.length - 1;
+    var guess;
+    while(min <= max) {
+        guess = Math.floor((max + min) / 2);
+         console.log(guess)
+        if (array[guess] === target) {
+            return "index"+guess ;
+        }
+        else if (array[guess] < target) {
+            min = guess + 1;
+        }
+        else {
+            max = guess - 1;
+        }
+
+    }
+
+    return -1;
+}
+
+
+var index = binarySearch([1, 2, 7, 15, 27], 27);
+console.log(index);
